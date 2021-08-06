fde5190471dd49566f2e4b52df7a398e66ce442b
komour <komaroff404@gmail.com>
2021-08-06 04:49:24 +0300

    Add CodingKeys for Size struct

--- MobileUpGallery/Models/Size.swift
+++ MobileUpGallery/Models/Size.swift
@@ -12,4 +12,20 @@ struct Size: Decodable {
     let width: Int
     let url: String
     let type: String
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.height = try container.decode(Int.self, forKey: .height)
+        self.width = try container.decode(Int.self, forKey: .width)
+        self.url = try container.decode(String.self, forKey: .url)
+        self.type = try container.decode(String.self, forKey: .type)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case height = "height"
+        case width = "width"
+        case url = "url"
+        case type = "type"
+    }
 }

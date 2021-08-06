c280b98c06932e47070f3bd0fb3ebbaa47583624
komour <komaroff404@gmail.com>
2021-08-06 03:53:25 +0300

    Fix indents

--- MobileUpGallery/Utils/UIImageView+load.swift
+++ MobileUpGallery/Utils/UIImageView+load.swift
@@ -8,28 +8,24 @@
 import UIKit
 
 extension UIImageView {
-  func loadImageUsingUrlString(urlString: String) {
-    image = #imageLiteral(resourceName: "placeholder")
-    
-    let url = URL(string: urlString)
-    guard let urlUnwrapped = url else {
-      print("nil url in \(#function)")
-      return
+    func loadImageUsingUrlString(urlString: String) {
+        let url = URL(string: urlString)
+        guard let urlUnwrapped = url else {
+            print("nil url in \(#function)")
+            return
+        }
+        URLSession.shared.dataTask(with: urlUnwrapped, completionHandler: { (data, _, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            guard let dataUnwrapped = data else {
+                print("nil data in \(#function)")
+                return
+            }
+            DispatchQueue.main.async {
+                self.image = UIImage(data: dataUnwrapped)
+            }
+        }).resume()
     }
-    
-    URLSession.shared.dataTask(with: urlUnwrapped, completionHandler: { (data, _, error) in
-      if let error = error {
-        print(error)
-        return
-      }
-      guard let dataUnwrapped = data else {
-        print("nil data in \(#function)")
-        return
-      }
-      
-      DispatchQueue.main.async {
-        self.image = UIImage(data: dataUnwrapped)
-      }
-      }).resume()
-  }
 }

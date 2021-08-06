5a3b41b32259a5fe0fd33385a007c855760dd1b5
komour <komaroff404@gmail.com>
2021-08-06 03:54:27 +0300

    Add success alert and date in title, increase height of the ImageView

--- MobileUpGallery/ShowPhoto/ShowPhotoViewController.swift
+++ MobileUpGallery/ShowPhoto/ShowPhotoViewController.swift
@@ -11,11 +11,10 @@ class ShowPhotoViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     var photoUrl: String?
+    var date: Double?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.title = "Date"
         
 //        share button setting
         let shareButton = UIBarButtonItem(image: #imageLiteral(resourceName: "share-button"), style: .plain, target: self, action: #selector(doShare))
@@ -28,6 +27,21 @@ class ShowPhotoViewController: UIViewController {
         } else {
             imageView.image = #imageLiteral(resourceName: "placeholder")
         }
+        
+//        title setting
+        setTitle()
+    }
+    
+    func setTitle() {
+        if let date = date {
+            let date = Date(timeIntervalSince1970: date)
+            let formater = DateFormatter()
+            formater.dateFormat = "MMM dd, yyyy"
+            self.title = formater.string(from: date)
+        } else {
+            self.title = "SEP 07, 2007"
+            print("nil date in \(#function)")
+        }
     }
     
     @objc func doShare() {
@@ -36,7 +50,15 @@ class ShowPhotoViewController: UIViewController {
             return
         }
         let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
-        // present the view controller
+        activityViewController.completionWithItemsHandler = { activity, success, items, error in
+            if activity == .saveToCameraRoll && success == true {
+                let alert = UIAlertController(title: nil, message: "Изображение успешно сохранено.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ок", style: .default, handler: { _ in
+                    alert.dismiss(animated: true, completion: nil)
+                }))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
         self.present(activityViewController, animated: true, completion: nil)
     }
 }
